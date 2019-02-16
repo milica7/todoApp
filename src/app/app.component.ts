5dc0c3f263a7e0ea06f061a91ae2afc5dce0f96f
milica7 <stanisic.milica7@gmail.com>
2019-02-16 16:58:08 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { trigger,animate,style,transition,keyframes} from '@angular/animations';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger("moveInLeft",[
+       transition("void=> *",[style({transform:"translateX(300px)"}),
+         animate(200,keyframes([
+          style({transform:"translateX(300px)"}),
+          style({transform:"translateX(0)"})
+  
+           ]))]),
+ 
+ 
+           transition("*=>void",[style({transform:"translateX(0px)"}),
+         animate(100,keyframes([
+          style({transform:"translateX(0px)"}),
+          style({transform:"translateX(300px)"})
+  
+           ]))])    
+      
+      ])
+ 
+   ]
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray=[];
+  todo;
+  // todoForm: new FormGroup();
+
+
+   addTodo(value){
+    if(value!==""){
+     this.todoArray.push(value)
+    // todoForm.resetForm()
+    //console.log(this.todos) 
+  }else{
+    alert('Field required **')
+  }
+    
+  }
+
+  /*delete item*/
+  deleteItem(todo){
+  			this.todoArray.splice(todo,1)
+  }
+
+  // submit Form
+  todoSubmit(value:any){
+     if(value!==""){
+    this.todoArray.push(value.todo)
+     //this.todoForm.reset()
+    }else{
+      alert('Field required **')
+    }
+    
+  }
+ 
 }
+
+
+
+
